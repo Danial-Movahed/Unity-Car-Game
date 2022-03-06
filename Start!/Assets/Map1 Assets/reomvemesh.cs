bd0897c6eb788d4e144b503383b1c0819d63a84c
Alireza <khosroshahi.alireza@yahoo.com>
2022-03-06 19:35:31 +0330

    last

---
+++ Start!/Assets/Map1 Assets/reomvemesh.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class reomvemesh : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        MeshCollider[] colliders = FindObjectsOfType<MeshCollider>();
+        int len = colliders.Length;
+        for( int i = 0; i < len; i++ ) GameObject.Destroy( colliders[i] );
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
