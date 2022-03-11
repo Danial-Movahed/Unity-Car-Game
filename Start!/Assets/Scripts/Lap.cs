0e7b7b43b9657c09ad25049bebd03808699b2d96
Seyed Danial Movahed <movaheddanial@gmail.com>
2022-03-11 07:23:26 +0330

    Added csproj files for vscode autocompletion and added lap system

---
+++ Start!/Assets/Scripts/Lap.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lap : MonoBehaviour
+{
+    public GameObject self;
+    private int lap = 0;
+    void OnTriggerEnter(Collider col)
+    {
+        Rigidbody rb = self.GetComponent<Rigidbody>();
+        if(rb.velocity.z > 0 && lap < 3)
+        {
+            lap++;
+            Debug.Log(lap);
+        }
+    }
+}
