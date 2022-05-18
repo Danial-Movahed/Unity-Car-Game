89e8fdaf15d434003747e2a042cdb5d559c23596
Seyed Danial Movahed <movaheddanial@gmail.com>
2022-05-18 09:02:51 +0430

    No one sees anything ?!

--- Start!/Assets/Scripts/Multiplayer/Client.cs
+++ Start!/Assets/Scripts/Multiplayer/Client.cs
@@ -33,7 +33,7 @@ public class Client : MonoBehaviour
             {
                 string[] tmp = dataReader.GetString(400).Split(' ');
                 GameObject.Find(tmp[0]).transform.position = new Vector3(float.Parse(tmp[1], CultureInfo.InvariantCulture.NumberFormat), float.Parse(tmp[2], CultureInfo.InvariantCulture.NumberFormat), float.Parse(tmp[3], CultureInfo.InvariantCulture.NumberFormat));
-                GameObject.Find(tmp[0]).transform.localEulerAngles = new Vector3(float.Parse(tmp[10], CultureInfo.InvariantCulture.NumberFormat), float.Parse(tmp[11], CultureInfo.InvariantCulture.NumberFormat), float.Parse(tmp[12], CultureInfo.InvariantCulture.NumberFormat));
+                GameObject.Find(tmp[0]).transform.localEulerAngles = new Vector3(float.Parse(tmp[4], CultureInfo.InvariantCulture.NumberFormat), float.Parse(tmp[5], CultureInfo.InvariantCulture.NumberFormat), float.Parse(tmp[6], CultureInfo.InvariantCulture.NumberFormat));
                 dataReader.Recycle();
             }
             else
