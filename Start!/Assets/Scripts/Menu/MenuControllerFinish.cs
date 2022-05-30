8d370a082bbdcad08df3d07c33c65bf1c1362a45
Seyed Danial Movahed <movaheddanial@gmail.com>
2022-05-30 08:48:43 +0430

    Fancy finish singleplayer (maybe)

--- Start!/Assets/Scripts/Menu/MenuControllerFinish.cs
+++ Start!/Assets/Scripts/Menu/MenuControllerFinish.cs
@@ -7,17 +7,9 @@ using UnityEngine.SceneManagement;
 public class MenuControllerFinish : MonoBehaviour
 {
     public Button okBtn;
-    public Text statusText;
-    private Config config;
     void Start()
     {
-        config = GameObject.Find("ConfigStart").GetComponent<Config>();
         okBtn.onClick.RemoveAllListeners();
-        if (config.selfName != "")
-        {
-            statusText.fontSize = 20;
-        }
-        statusText.text = "Finished\n" + config.message;
         okBtn.onClick.AddListener(() =>
         {
             Debug.Log("ok");
